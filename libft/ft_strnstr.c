80f4147323cc4603ff0ce1ccd7cb17c5be5cbae9
Pedro Henrique barbosa domingues <pehenriq@c1r3p1.42sp.org.br>
2021-06-15 13:23:07 -0300

    taking restrict off

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,37 +6,42 @@
 /*   By: pehenriq <pehenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 20:41:23 by pehenriq          #+#    #+#             */
-/*   Updated: 2021/06/06 18:42:58 by pehenriq         ###   ########.fr       */
+/*   Updated: 2021/06/15 13:22:32 by pehenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
-{
-	size_t	i;
-	size_t	j;
-	char	*str;
+char	*ft_strnstr(const char *haystack, const char *needle,  (needle[0] == '\0')
 
-	str = (char *)big;
-	if (big[0] == '\0')
-		return (NULL);
-	if (little[0] == '\0')
-		return (str);
-	i = 0;
-	while (str[i] != '\0' && i < len)
-	{
-		j = 0;
-		if (str[i] == little[j])
-		{
-			while (str[i + j] == little[j] && i + j < len && little[j] != '\0')
-			{
-				j++;
-			}
-			if (little[j] == '\0')
-				return (&str[i]);
-		}
-		i++;
-	}
-	return (0);
+								return ((char *)haystack);
+
+						while (haystack[h] != '\0')
+
+								{
+
+											n = 0;
+
+													while (haystack[h + n] == needle[n] && (h + n) < len)
+
+																{
+
+																				if (haystack[h + n] == '\0' && needle[n] == '\0')
+
+																									return ((char *)&haystack[h]);
+
+																							n++;
+
+																									}
+
+															if (needle[n] == '\0')
+
+																			return ((char *)haystack + h);
+
+																	h++;
+
+																		}
+
+							return (0);
+
 }
