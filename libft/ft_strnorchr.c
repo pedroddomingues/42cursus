55b779a064ab68a2580705aad7d447e05df055ef
Pedro H Domingues <pedroddomingues@gmail.com>
2022-02-13 20:56:19 +0100

    libft norm check

--- libft/ft_strnorchr.c
+++ libft/ft_strnorchr.c
@@ -6,13 +6,13 @@
 /*   By: pehenriq <pehenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 20:35:50 by pehenriq          #+#    #+#             */
-/*   Updated: 2021/06/11 23:26:02 by pehenriq         ###   ########.fr       */
+/*   Updated: 2022/02/13 19:26:37 by pehenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnorchr(const char	*s, int	c)
+char	*ft_strnorchr(const char *s, int c)
 {
 	size_t	size;
 	char	*str;
