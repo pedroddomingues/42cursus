50fe27665ec98cd4e6fdb8e117ea87041660447a
Pedro H Domingues <pedroddomingues@gmail.com>
2022-02-18 02:53:24 +0100

    fdf NOW

--- fdf/src/exit.c
+++ fdf/src/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pehenriq <pehenriq@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 16:44:54 by pehenriq          #+#    #+#             */
-/*   Updated: 2022/02/13 21:39:14 by pehenriq         ###   ########.fr       */
+/*   Updated: 2022/02/18 02:49:26 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,29 @@ static void	free_map(t_fdf_params *fdf)
 	while (i <= fdf->map.x_max)
 	{
 		free(fdf->map.points[i]);
-		free(fdf->map.colors[i]);
 		i++;
 	}
 	free(fdf->map.points);
-	free(fdf->map.colors);
+}
+
+static void	exit_code1(t_fdf_params *fdf, int code)
+{
+	ft_printf("Error reading file\n");
+	free(fdf);
+	exit(code);
+}
+
+static void	exit_code2(t_fdf_params *fdf, int code)
+{
+	ft_printf("Error reading file\n");
+	free(fdf->angles);
+	free(fdf->map.path);
+	mlx_clear_window(fdf->mlx, fdf->win);
+	mlx_destroy_window(fdf->mlx, fdf->win);
+	mlx_destroy_display(fdf->mlx);
+	free(fdf->mlx);
+	free(fdf);
+	exit(code);
 }
 
 void	exit_program(t_fdf_params *fdf, int code)
@@ -34,14 +52,9 @@ void	exit_program(t_fdf_params *fdf, int code)
 	else if (code == -1)
 		ft_printf("Error. Exiting program.\n");
 	else if (code == 1)
-	{
-		ft_printf("Error reading file\n");
-		free(fdf);
-		exit(code);
-	}
+		exit_code1(fdf, code);
 	else if (code == 2)
-		ft_printf("Error reading file\n"
-			"This file is not an fdf file.\n");
+		exit_code2(fdf, code);
 	free(fdf->angles);
 	free(fdf->map.path);
 	free_map(fdf);
--- fdf/src/map.c
+++ fdf/src/map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pehenriq <pehenriq@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 23:01:12 by pehenriq          #+#    #+#             */
-/*   Updated: 2022/02/13 21:31:11 by pehenriq         ###   ########.fr       */
+/*   Updated: 2022/02/18 02:49:03 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,10 @@ static void	map_malloc(t_fdf_params *fdf, int fd)
 	fdf->map.points = (int **)malloc(sizeof(int *) * (fdf->map.x_max + 1));
 	if (!fdf->map.points)
 		error(1, 0, "Error while allocating memory.");
-	fdf->map.colors = (int **)malloc(sizeof(int *) * (fdf->map.x_max + 1));
-	if (!fdf->map.colors)
-		error(1, 0, "Error while allocating memory.");
 	while (i <= fdf->map.x_max)
 	{
 		fdf->map.points[i] = (int *)malloc(sizeof(int) * (fdf->map.y_max + 1));
 		if (!fdf->map.points[i])
-			error(1, 0, "Error while allocating memory.");
-		fdf->map.colors[i] = (int *)malloc(sizeof(int) * (fdf->map.y_max + 1));
-		if (!fdf->map.colors[i])
 			error(1, 0, "Error while allocating memory.");
 		i++;
 	}
@@ -63,7 +57,6 @@ static void	map_malloc(t_fdf_params *fdf, int fd)
 static void	parse_line(t_fdf_params *fdf, char *line)
 {
 	char	**line_split;
-	char	**color_split;
 	int		y;
 	int		z;
 
@@ -71,12 +64,6 @@ static void	parse_line(t_fdf_params *fdf, char *line)
 	line_split = ft_split(line, ' ');
 	while (line_split[y])
 	{
-		if (ft_strchr(line_split[y], ','))
-		{
-			color_split = ft_split(line_split[y], ',');
-			fdf->map.colors[fdf->map.x][y] = ft_hexstrtoi(color_split[1]);
-			ft_free_split(color_split);
-		}
 		z = ft_atoi(line_split[y]);
 		fdf->map.points[fdf->map.x][y] = z;
 		if (z > fdf->map.z_max)
@@ -96,7 +83,7 @@ void	load_map(t_fdf_params *fdf)
 
 	fd = open(fdf->map.path, O_RDONLY, 0);
 	if (fd == -1)
-		exit_program(fdf, 1);
+		exit_program(fdf, 2);
 	map_malloc(fdf, fd);
 	close(fd);
 	fd = open(fdf->map.path, O_RDONLY, 0);
